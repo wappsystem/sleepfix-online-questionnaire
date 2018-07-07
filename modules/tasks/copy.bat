69c906361f69144f2132316b86970c4ca677b28a
Zhiming Zhou <zzm.vmiis@gmail.com>
2018-07-07 18:41:22 +1000

    update

--- modules/tasks/copy.bat
+++ modules/tasks/copy.bat
@@ -1,3 +1,11 @@
-xcopy %USERPROFILE%\Documents\GitHub\wappsystem\sleepfix-management\modules\tasks\task-form.js               %USERPROFILE%\Documents\GitHub\wappsystem\sleepfix-online-questionnaire\modules\tasks\mirror\task-form.js /D
-xcopy %USERPROFILE%\Documents\GitHub\wappsystem\sleepfix-management\modules\tasks\bl-questionnaire-form.html %USERPROFILE%\Documents\GitHub\wappsystem\sleepfix-online-questionnaire\modules\tasks\mirror\bl-questionnaire-form.html /D
+set S=%USERPROFILE%\Documents\GitHub\wappsystem\sleepfix-management\modules\tasks
+set D=%USERPROFILE%\Documents\GitHub\wappsystem\sleepfix-online-questionnaire\modules\tasks\mirror
+
+xcopy %S%\task-form.js %D%\task-form.js /D /Y
+xcopy %S%\bl-questionnaire-form.html %D%\bl-questionnaire-form.html /D /Y
+xcopy %S%\ess-s2-f-form.html %D%\ess-s2-f-form.html /D /Y
+xcopy %S%\isi-form.html %D%\isi-form.html /D /Y
+
+
+
 pause
